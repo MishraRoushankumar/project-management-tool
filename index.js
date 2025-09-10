6f929fcddaeb5d830484cc9868832a957b2574be
Roushankumar Mishra <mishraroushan455@gmail.com>
2025-09-10 19:46:29 +0530

    update: dotenv addition

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const myApiKey = process.env.API_KEY;
+
+console.log("value : ", myApiKey);
+
 console.log("start of an awesome backened project");
