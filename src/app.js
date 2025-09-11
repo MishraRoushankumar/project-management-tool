18e0816cbd908437fa672d5585b5e58b916b0f64
Roushankumar Mishra <mishraroushan455@gmail.com>
2025-09-12 00:27:29 +0530

    Auth: login feature implemented

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // CORS configurations
 app.use(
@@ -19,7 +21,6 @@ app.use(
 );
 
 // import the routes
-
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
 
